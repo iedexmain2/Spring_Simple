840b525e0d489ee84eac647fc5f861c0a19ea3b4
artaleks9@gmail.com <artaleks9@gmail.com>
2014-10-24 14:49:53 +0000

    qqqq

--- src/main/java/com/codenvy/example/spring/GreetingController.java
+++ src/main/java/com/codenvy/example/spring/GreetingController.java
@@ -1,5 +1,5 @@
 package com.codenvy.example.spring;
-
+//  change
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
---
+++ src/main/java/com/codenvy/example/spring/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.codenvy.example.spring;
+
+public class HelloWorld {
+    
+    public static void main(String[] arg){
+        System.out.println("Hello,World!!1");
+        
+    }
+}
